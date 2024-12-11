fb371907ab97ffac1439f4e82dc719fae4a0f8c6
jsekad <jsekad@gmail.com>
2024-12-12 00:13:22 +0200

    Spitogatos Tests

--- src/test/java/dev/slnm/loginTests/TestAtLogin.java
+++ src/test/java/dev/slnm/loginTests/TestAtLogin.java
@@ -1,11 +1,12 @@
 package dev.slnm.loginTests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.List;
 
 public class TestAtLogin {
     @Test
@@ -20,6 +21,23 @@ public class TestAtLogin {
         submitButton.click();
         driver.quit();
     }
+
+    @Test
+    void select_spitogatos_kolonos_ampelokipoi(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.get("https://www.spitogatos.gr/pwliseis-katoikies/pollaples_perioxes-2051,2007/timi_apo-60000/timi_eos-80000/emvado_apo-55");
+        List<WebElement> webElements = driver.findElements(By.xpath( "//a[contains(@href, '/aggelia/') and contains(@class, 'tile__link')]"));
+        for (WebElement webElement : webElements) {
+
+            String href = webElement.getAttribute("href");
+            ((JavascriptExecutor) driver).executeScript("window.open(arguments[0], '_blank');", href);
+        }
+
+
+    }
+
 
 
 }
